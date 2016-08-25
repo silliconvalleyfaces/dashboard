c29130571c8a76b245f0d80090bfff30b358a8ae
Deandreharvey <deandre.mharvey@gmail.com>
2016-08-25 16:12:19 -0700

    added comment model

--- server/models/user.js
+++ server/models/user.js
@@ -3,13 +3,54 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new mongoose.Schema({
-    first_name: String,
-    last_name : String,
-    email : String,
-    password : String,
-    phone : Number,
-    profile_pic : String
-});
+    first_name: {
+      type:  String,
+      required : true,
+      maxlength : 25
+    },
+    last_name : {
+      type : String,
+      required : true,
+      maxlength : 25
+    },
+    email : {
+     type : String,
+     required : true,
+     maxlength : 25
+   },
+   email_share : {
+    type : Boolean
+  },
+    password : {
+      type :  String,
+      required : true
+    },
+    phone : {
+      type : Number,
+      required : true,
+    },
+    phone_share : {
+     type : Boolean
+   },
+    profile_pic : {
+      type : String,
+      required : true
+    },
+    user_level : {
+      type : Number,
+      required : true
+    },
+    _post_id: [{
+      type: Schema.Types.ObjectId,
+      ref: 'posts'
+    }],
+    _comment_id: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }]
+  },
+    {timestamps: true}
+);
 
 // the following codes are for passport
 // they need to be above mongoose.model('users', UserSchema);
