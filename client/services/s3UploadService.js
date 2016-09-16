f5ebd97cfbf767f1bc2a7dafd56e654b4dc938bd
jia <edison7476@hotmail.com>
2016-09-16 12:03:46 -0700

    remove keys

--- client/services/s3UploadService.js
+++ client/services/s3UploadService.js
@@ -1,7 +1,7 @@
 myApp.service('S3UploadService', ['$q', function ($q) {
     // Us standard region
     AWS.config.region = 'us-west-1';
-    AWS.config.update({ accessKeyId: 'AKIAJT5OOKBUZTKC4FCQ', secretAccessKey: 'YAKBRfvwgcCmWsuzQ/VtS7jep8FHwhjgxsA/ry1o' });
+    AWS.config.update({ accessKeyId: 'fake', secretAccessKey: 'fake' });
 
     var bucket = new AWS.S3({ params: { Bucket: 'siliconvalleyfaces', maxRetries: 5 }, httpOptions: { timeout: 360000 } });
 
