1eb9509c83d72d8eac56d9733acd258c35cd7811
Andrew Yeung <andrewhyeung@gmail.com>
2016-09-15 22:43:14 -0700

    implementing crop feature

--- client/services/s3UploadService.js
+++ client/services/s3UploadService.js
@@ -1,12 +1,14 @@
 myApp.service('S3UploadService', ['$q', function ($q) {
     // Us standard region
     AWS.config.region = 'us-west-1';
-    AWS.config.update({ accessKeyId: 'key', secretAccessKey: 'key' });
+    AWS.config.update({ accessKeyId: 'fake', secretAccessKey: 'fake' });
 
     var bucket = new AWS.S3({ params: { Bucket: 'siliconvalleyfaces', maxRetries: 5 }, httpOptions: { timeout: 360000 } });
 
     this.Progress = 0;
     this.Upload = function (file) {
+        console.log('in the S3UploadService')
+        console.log(file)
         var deferred = $q.defer();
         var params = { Bucket: 'siliconvalleyfaces', Key: file.url, ContentType: file.type, Body: file };
         var options = {
